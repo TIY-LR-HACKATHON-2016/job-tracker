045fb6ce9f9ba8c932bf67f0d032d9e2755067b8
Tee Link <tesuanseylink@gmail.com>
2016-04-15 21:10:37 -0500

    user model

---
+++ server/JobTracker/JobTracker.Web/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobTracker.Web.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Resume { get; set; }
+        public string CoverLetter { get; set; }
+        public string Address { get; set; }
+        public string PhoneNum { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
