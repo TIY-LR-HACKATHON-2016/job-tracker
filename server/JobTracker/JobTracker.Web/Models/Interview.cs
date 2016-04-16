88b2c9d4a0488a682ab92e06a41b3a6432644fd8
kvnmach <kvnmach@gmail.com>
2016-04-15 21:40:27 -0500

    job and interview property created

---
+++ server/JobTracker/JobTracker.Web/Models/Interview.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+
+namespace JobTracker.Web.Models
+{
+    public class Interview
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Job Job { get; set; }
+        public User User  { get; set; }
+    }
+}
---
+++ server/JobTracker/JobTracker.Web/Models/Job.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace JobTracker.Web.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        [Required]
+        public string CompanyTitle { get; set; }
+        public string Url { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string JobTitle { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public virtual ICollection<Interview> Interviews { get; set; } = new List<Interview>();
+    }
+}
